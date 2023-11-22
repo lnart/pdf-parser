1daca88c4482c50afa91498274196c60e2661a8b
Lennart <115736797+lnart@users.noreply.github.com>
2023-11-22 21:54:09 +0100

    FEAT: create route to read uploaded pdf files, returns filenames and parsed text

--- server/src/router/uploadRoutes.ts
+++ server/src/router/uploadRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import fs from 'fs'
+import path from 'path'
 import pdfParse from 'pdf-parse'
 const router = Router()
-import { upload } from "..//multerConfig/multer";
+import { upload, uploadsdir} from "..//multerConfig/multer";
 import { config } from "dotenv";
 
 config()
@@ -15,3 +16,22 @@ router.post('/api/v1/upload', upload.single('file'), async(req, res) => {
       })
     }
   })
+
+  router.get('/api/v1/uploads', async (req, res) => {
+    try {
+      const files = fs.readdirSync(uploadsdir);
+      const pdfFiles = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
+  
+      const fileData = await Promise.all(pdfFiles.map(async (file) => {
+        const filePath = path.join(uploadsdir, file);
+        const dataBuffer = fs.readFileSync(filePath);
+        const data = await pdfParse(dataBuffer);
+        return { filename: file, text: data.text };
+      }));
+  
+      res.json(fileData);
+    } catch (err) {
+      console.error('Error reading files:', err);
+      res.status(500).send('Error reading files');
+    }
+  });
